f12c588914af340d9b5871b407fcc020df0c2d21
marwadesoky810 <marwadesoky76@gmail.com>
2023-12-09 04:28:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+  var quotes = [
+    {
+      quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+      author: "Franklin D. Roosevelt"
+    },
+    {
+      quote: "Believe you can and you're halfway there.",
+      author: "Theodore Roosevelt"
+    },
+    {
+      quote: "The future belongs to those who believe in the beauty of their dreams.",
+      author: "Eleanor Roosevelt"
+    },
+    {
+        quote:"You only live once, but if you do it right, once is enough.",
+        author:" Mae West"
+    },
+    {
+        quote:"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+        author:"J.K. Rowling"
+    },
+    {
+        quote:"If you tell the truth, you don't have to remember anything.",
+        author:" Mark Twain"
+    },
+    {
+        quote:"A friend is someone who knows all about you and still loves you.",
+        author:" Elbert Hubbard"
+    },
+    {
+        quote:"Always forgive your enemies; nothing annoys them so much.",
+        author:" Oscar Wilde"
+    },
+    {
+        quote:"Live as if you were to die tomorrow. Learn as if you were to live forever.",
+        author:" Mahatma Gandhi"
+    },
+    {
+        quote:"Be yourself; everyone else is already taken.",
+        author:" Oscar Wilde"
+    },
+   
+   
+  ];
+
+var lastRandomIndex = -1;
+var buttonClicked = false;
+var randomIndex;
+
+function generateQuote() {
+  if (!buttonClicked) {
+    buttonClicked = true;
+    return;
+  }
+  do {
+    randomIndex = Math.floor(Math.random() * quotes.length);
+  } 
+  while (randomIndex === lastRandomIndex);
+
+  lastRandomIndex = randomIndex;
+
+  var randomQuote = quotes[randomIndex];
+
+  document.getElementById("Quote").innerHTML = `"${randomQuote.quote}"`;
+  document.getElementById("auther").innerHTML = `- ${randomQuote.author}`;
+}
+
+generateQuote();
